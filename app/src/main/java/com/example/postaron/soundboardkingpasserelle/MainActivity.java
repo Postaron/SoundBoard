b0faad09335d1f217961bb45a74a2cc57b621656
postaron <florent.denef@gmail.com>
2017-11-01 10:48:58 +0100

    Ajout des Paramètres. Support mail fini. NavHeader non fini. Paramètres non fini. UI Fragment.

--- app/src/main/java/com/example/postaron/soundboardkingpasserelle/MainActivity.java
+++ app/src/main/java/com/example/postaron/soundboardkingpasserelle/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.postaron.soundboardkingpasserelle;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +14,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+/**
+ * @author Florent Denef
+ * @version 0.1
+ */
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TextView email = null;
+    private Fragment fragment = null;
+    private Class fragmentClass = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,17 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fragmentClass = MainFragment.class;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flContent, fragment)
+                .commit();
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -31,6 +54,26 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        email = navigationView.getHeaderView(0).findViewById(R.id.supportMail);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emailSupport();
+            }
+        });
+    }
+
+    /**
+     * It adds the possibility to prepare an e-mail by clicking on the e-mail address.
+     */
+    private void emailSupport() {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);//Intention to send something
+        intent.setData(Uri.parse("mailto:"));//only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email.getText().toString()});//Prepare the mail by adding the address
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Support : Sounboard KingPasserelle");
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);//Start the e-mail client
     }
 
     @Override
