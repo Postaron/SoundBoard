699d2fc44c9ed20393253672aac76aebbf3e6fb8
postaron <florent.denef@gmail.com>
2017-11-10 17:30:56 +0100

    Début de la simplification du SoundPool

---
+++ app/src/main/java/LecteurSon/Lecteur.java
@@ -0,0 +1,14 @@
+package LecteurSon;
+
+import android.media.SoundPool;
+
+/**
+ * Will be used to simplify the usage of SoundPool class
+ *
+ * @author postaron
+ * @version 1.0
+ */
+
+public class Lecteur {
+    private SoundPool soundPool = null; //First step
+}
