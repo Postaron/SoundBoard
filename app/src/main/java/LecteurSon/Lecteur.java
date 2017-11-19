7c8ca21a858a1191cf22842f0bd3bf3edac86d7c
Postaron <florent.denef@gmail.com>
2017-11-19 16:38:18 +0100

    Début création Lecteur et UI de la SoundBoard. Écriture des solutions en commentaire dans la classe Lecteur.

--- app/src/main/java/LecteurSon/Lecteur.java
+++ app/src/main/java/LecteurSon/Lecteur.java
@@ -11,4 +11,12 @@ import android.media.SoundPool;
 
 public class Lecteur {
     private SoundPool soundPool = null; //First step
+    /*
+    Autres solutions :
+    - mettre tous les morceaux en raw, pourquoi pas une liste de raw ressource contenant tous les
+    morceaux
+    - ajout possible de fichiers au fur et à mesure (au bout d'un moment, on remet en dur dans un
+    autre APK)
+    - ajout des morceaux dans un dossier asset possible : ajout par la suite possible aussi.
+     */
 }
--- app/src/main/java/com/example/postaron/soundboardkingpasserelle/MainFragment.java
+++ app/src/main/java/com/example/postaron/soundboardkingpasserelle/MainFragment.java
@@ -5,9 +5,12 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import LecteurSon.Lecteur;
 
 
 /**
@@ -20,6 +23,7 @@ import android.view.ViewGroup;
  */
 public class MainFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
+    private Lecteur lecteur = null;
 
     public MainFragment() {
         // Required empty public constructor
@@ -49,6 +53,12 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 
     @Override
